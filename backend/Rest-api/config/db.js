0889c50f370d01faea6a8c8f6d9daf88ae6cf409
Nikolay Todorov <nikolay.b.todorov@gmail.com>
2026-03-31 03:48:19 +0300

    Delete backend/Rest-api directory

--- backend/Rest-api/config/db.js
+++
@@ -1,7 +0,0 @@
-const config = require('./config');
-const mongoose = require('mongoose');
-
-module.exports = () => {
-  console.log('Connecting to:', config.dbURL);
-  return mongoose.connect(config.dbURL);
-};
