0a4f2d29faad2e5240da9f5488532b7ab2252c47
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2026-04-02 04:29:57 +0300

    backend changes

--- 07.State Management/project/src/app/app.routes.ts
+++ 07.State Management/project/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { LoginComponent } from './features/login/login.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
 import { MissionDetailsComponent } from './features/mission-details/mission-details.component';
 import { missionResolver } from './guards/mission.resolver';
-import { CommandCenterComponent } from './features/command-center/command-center.component';
 import { authGuard } from './guards/auth.guard';
 import { CrewRegisterComponent } from './features/crew-register/crew-register.component';
 import { MissionFormComponent } from './features/mission-form/mission-form.component';
